e1a05d01346648378f17925ad8e5a07ad068f57f
unknown <guilhermemilhoratoferreira@gmail.com>
2020-08-05 20:25:29 -0300

    Exercicios Capitulo 03

---
+++ cap03-exerc03.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	float nota1, nota2, nota3, peso1, peso2, peso3, media;
+	
+	printf("Informe a primeira nota: ");
+	scanf("%f", &nota1);
+	printf("Informe o peso da primeira nota: ");
+	scanf("%f", &peso1);
+	
+	printf("Informe a segunda nota: ");
+	scanf("%f", &nota2);
+	printf("Informe o peso da segunda nota: ");
+	scanf("%f", &peso2);
+	
+	printf("Informe a terceira nota: ");
+	scanf("%f", &nota3);
+	printf("Informe o peso da terceira nota: ");
+	scanf("%f", &peso3);
+	
+	media = ((nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / peso1+peso2+peso3);
+	
+	printf("A media das notas e %.2f", media);
+	
+	return 0;
+	
+}
